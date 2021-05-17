a5e70fc96f6c8fc2565bf6b74de74ec6b95c8c98
JasonW <jwei0415@gmail.com>
2021-05-16 20:03:34 -0700

    add user account function: login/logout

--- mysite/user/urls.py
+++ mysite/user/urls.py
@@ -5,4 +5,7 @@ app_name = 'user'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.my_login, name='login'),
+    path('logout', views.my_logout, name='logout'),
+    path('signup', views.my_signup, name='signup'),
 ]
--- mysite/user/views.py
+++ mysite/user/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the user index.")
+
+
+def my_signup(request):
+    if request.user.is_authenticated:
+        return render(request, 'user/signup.html', None)
+    if request.method == 'POST':
+        post_dict = request.POST
+        username = post_dict.get("username", "")
+        email = post_dict.get("email", "")
+        password = post_dict.get("password", "")
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        return render(request, 'user/login.html', None)
+    else:
+        return render(request, 'user/signup.html', None)
+
+
+def my_login(request):
+    if request.method == 'POST':
+        post_dict = request.POST
+        username = post_dict.get("username", "")
+        password = post_dict.get("password", "")
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'user/login.html')
+    else:
+        return render(request, 'user/login.html')
+
+
+def my_logout(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return render(request, 'user/logout.html')
