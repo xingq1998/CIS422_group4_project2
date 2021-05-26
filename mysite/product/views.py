90d05780bf8302e420abcf2db8671447f5ff1642
Ian Parish <iparish@uoregon.edu>
2021-05-25 22:38:19 -0700

    Implemented product filter search functionality and updated RESET FILTERS button to work

--- mysite/product/views.py
+++ mysite/product/views.py
@@ -3,14 +3,40 @@ from .models import Item
 
 # Create your views here.
 
+
 def all(request):
     categories = []
     products = Item.objects.all()
 
     for prod in products:
         if prod.category not in categories:
-            print(prod.category)
+            #print(prod.category)
             categories.append(prod.category)
 
-    print(categories)
+    #print(categories)
     return render(request, 'product/all.html', {"products" : products, "categories" : categories})
+
+
+#---------- Search Functions ----------#
+
+
+def product_search(request):
+    products = Item.objects.all()
+    categories = []
+
+    for prod in products:
+            if prod.category not in categories:
+                categories.append(prod.category)
+
+    if request.method == 'POST':
+        post_dict = request.POST
+        search_cat = post_dict.get("category", "")
+
+        products = Item.objects.all().filter(category=search_cat)
+
+        return render(request, 'product/all.html', {"products" : products, "categories" : categories})
+
+    else:
+               
+        return render(request, 'product/all.html', {"products" : products, "categories" : categories})
+
