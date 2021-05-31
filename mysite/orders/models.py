5c27a7ae14b6dfe596b562c8d261dc2ad9c8b561
Xingq <59669726+xingq1998@users.noreply.github.com>
2021-05-31 23:10:32 +0800

    add order model comments

--- mysite/orders/models.py
+++ mysite/orders/models.py
@@ -6,6 +6,15 @@ from django.contrib import admin
 
 
 class Order(models.Model):
+    """
+    Order Model: The order being created in cart module. Accessible through the
+                django Model framework.
+
+    Data Fields:
+        id: The id of the order records.
+        user_id: The id of the user who created the order.
+        total_cost: The total_cost of the order.
+    """
     user_id = models.IntegerField(default=0)
     total_cost = models.FloatField(default=0.0)
 
@@ -19,6 +28,21 @@ class Order(models.Model):
 
 
 class OrderedItem(models.Model):
+    """
+    OrderedItem Model: The items contains in each order. Accessible through the
+                django Model framework.
+
+    Data Fields:
+        product_id: The product id of the order item when checking out.
+        product_name: The name of the order item when checking out.
+        product_category: The product category of the order item when checking out.
+        product_pic_address: The pic_address of the order item when checking out.
+        quantity: The quantity of the order items when checking out.
+        cart_item: The foreign key of the CartItem class that the OrderedItem instance relates to.
+        price: The product price of the order item when checking out.
+        order: The foreign key of the Order class that the OrderedItem instance relates to.
+        created_at: The created timestamp when user created the order.
+    """
     product_id = models.IntegerField(default=1)
     product_name = models.CharField(max_length=300, default="")
     product_category = models.CharField(default="N/A", max_length=20)
